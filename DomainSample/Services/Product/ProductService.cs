afd4a7e3b83966d56454cb52641d350dee993588
Erick Pacheco <erick.pacheco-brito@snhu.edu>
2022-03-14 21:56:50 -0500

    Implemented Create, FindByID, and Delete

--- DomainSample/Services/Product/ProductService.cs
+++ DomainSample/Services/Product/ProductService.cs
@@ -2,6 +2,7 @@
 using DomainSample.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DomainSample.Services.Product
 {
@@ -11,7 +12,28 @@ namespace DomainSample.Services.Product
 
         public ProductDto CreateProduct(CreateProductRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                EntryLog(_logArea, request);
+
+                if (request == null) throw new ArgumentException("Null Request");
+
+                var model = new Models.Product
+                {
+                    Description = request.Description,
+                    CurrentPrice = request.CurrentPrice
+                };
+
+                _context.Products.Add(model);
+                _context.SaveChanges();
+
+                return new ProductDto(model);
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(_logArea, ex);
+                throw ex;
+            }
         }
 
         public ProductDto UpdateProduct(int id, UpdateProductRequest request)
@@ -21,12 +43,42 @@ namespace DomainSample.Services.Product
 
         public int DeleteProductById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                EntryLog(_logArea, id);
+
+                var model = _context.Products.Where(p => p.Id == id).FirstOrDefault();
+
+                if (model == null) throw new ArgumentException("Product Not Found");
+
+                _context.Products.Remove(model);
+
+                _context.SaveChanges();
+
+                return id;
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(_logArea, ex);
+                throw ex;
+            }
         }
 
         public ProductDto GetProductById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var model = _context.Products.Where(p => p.Id == id).FirstOrDefault();
+
+                if (model == null) throw new ArgumentException("Product Not Found");
+
+                return new ProductDto(model);
+            }
+            catch (Exception ex)
+            {
+                ErrorLog(_logArea, ex);
+                throw ex;
+            }
         }
 
         public List<ProductDto> FindProducts(FindProductsRequest request)
