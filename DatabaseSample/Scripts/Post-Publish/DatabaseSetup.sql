8cdce63ceee81914830aeca1659ff70f66fdf938
MaxBeauchemin <maxbeauchemin@gmail.com>
2022-03-14 21:16:28 -0500

    Actually fix the script (please)

--- DatabaseSample/Scripts/Post-Publish/DatabaseSetup.sql
+++ DatabaseSample/Scripts/Post-Publish/DatabaseSetup.sql
@@ -13,11 +13,11 @@
 IF NOT EXISTS (SELECT name FROM master.sys.server_principals WHERE name = 'db_rw')
 BEGIN
 
-    CREATE LOGIN db_rw WITH PASSWORD = 'P4$$W0RD'
+    CREATE LOGIN db_rw_login WITH PASSWORD = 'P4$$W0RD'
 
 END
 
-IF NOT EXISTS (SELECT name FROM master.sys.server_principals WHERE name = 'db_rw_user')
+IF NOT EXISTS (SELECT name FROM DatabaseSample.sys.database_principals WHERE name = 'db_rw_user')
 BEGIN
 
     CREATE USER db_rw_user FOR LOGIN db_rw
